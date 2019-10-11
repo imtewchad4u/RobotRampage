9defa636b4b921909416ab7dee611d8b1c0d9405
Chad Bissonnette [Student] <chad.bissonnette@student.sl.on.ca>
2019-10-11 13:52:32 -0400

    Firing Robot Missiles

--- Assets/Robot.cs
+++ Assets/Robot.cs
@@ -6,6 +6,8 @@ public class Robot : MonoBehaviour
 {
     [SerializeField]
     private string robotType;
+    [SerializeField]
+    GameObject missileprefab;
 
     public int health;
     public int range;
@@ -46,6 +48,9 @@ public class Robot : MonoBehaviour
     }
     private void fire()
     {
+        GameObject missile = Instantiate(missileprefab);
+        missile.transform.position = missileFireSpot.transform.position;
+        missile.transform.rotation = missileFireSpot.transform.rotation;
         robot.Play("Fire");
     }
 
---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed = 30f;
+    public int damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("deathTimer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    IEnumerator deathTimer()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(gameObject);
+    }
+}
