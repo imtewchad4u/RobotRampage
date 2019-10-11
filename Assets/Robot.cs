26425d7e854c2e00a3ea4ee20012032a229a33a0
Chad Bissonnette [Student] <chad.bissonnette@student.sl.on.ca>
2019-10-11 13:34:16 -0400

    Adding the Robots

--- Assets/Robot.cs
+++ Assets/Robot.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
+    [SerializeField]
+    private string robotType;
+
+    public int health;
+    public int range;
+    public float fireRate;
+
+    public Transform missileFireSpot;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    private Transform player;
+    private float timeLastFired;
+
+    private bool isDead;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isDead = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isDead)
+        {
+            return;
+        }
+        transform.LookAt(player);
+        agent.SetDestination(player.position);
+
+        if (Vector3.Distance(transform.position, player.position) < range 
+            && Time.time - timeLastFired > fireRate)
+        {
+            timeLastFired = Time.time;
+            fire();
+        }
+    }
+    private void fire()
+    {
+        Debug.Log("Fire");
     }
 }
