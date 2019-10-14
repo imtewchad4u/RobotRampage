6e17cb54486dea2427ed09689625bc975b20ea3c
unknown <imtewchad4u@outlook.com>
2019-10-14 10:28:10 -0400

    More Spawning Logic

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -17,6 +17,7 @@ public class Pickup : MonoBehaviour
             && collider.gameObject.tag == "Player")
         {
             collider.gameObject.GetComponent<Player>().PickUpItem(type);
+            GetComponentInParent<PickupSpawn>().PickupWasPickedUp();
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/PickupSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pickups;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPickup();
+    }
+
+    void spawnPickup()
+    {
+        GameObject pickup = Instantiate(pickups[Random.Range(0, pickups.Length)]);
+        pickup.transform.position = transform.position;
+        pickup.transform.parent = transform;
+    }
+
+    IEnumerator respawnPickup()
+    {
+        yield return new WaitForSeconds(20);
+        spawnPickup();
+    }
+
+    public void PickupWasPickedUp()
+    {
+        StartCoroutine("respawnPickup");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
